baadffa59a19770b8b972f595f9485b7aa289a3d
IreneGaoc <zgao1@ualberta.ca>
2019-02-07 18:06:56 -0700

    lab

--- comput lab5 demo/pelicanconf.py
+++ comput lab5 demo/pelicanconf.py
@@ -14,10 +14,11 @@ DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
+#CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
+THEME = 'templates/mytheme'
 
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
